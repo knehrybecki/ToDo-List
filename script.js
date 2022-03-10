ed59766382c040c43a89718312068f589e0697a5
Kamil Nehrybecki <knehrybecki@gmail.com>
2022-03-10 21:36:16 +0100

    corrected Todo

--- script.js
+++ script.js
@@ -1,75 +1,60 @@
 const inputText = document.querySelector('.input-text')
-const addItem = document.querySelector('.add-item')
-const myList = document.querySelector('.todo__list')
+const buttonAddTodo = document.querySelector('.add-item')
+const todoList = document.querySelector('.todo__list')
 
-const buy = []
+let todo = []
 
- listItem = () => {
-    const listBuy = document.createElement('li')
-    listBuy.classList.add('todo__item')
+const createNewTodo = () => {
+    const list = document.createElement('li')
+    list.classList.add('todo__item')
+    list.textContent = inputText.value       
 
-    buy.forEach((value) => {
-        listBuy.textContent = value.buy
-        myList.appendChild(listBuy)  
-    })
+    return list
 }
 
-buttonAddItem = () => {
-    addItem.addEventListener("click", (event) =>{ 
-        if(inputText.value === "") {return alert("Please write any text")}
-        else {buy[buy.length] = {buy : inputText.value}}
-        console.log(buy)
-        event.preventDefault()
-        listItem()
-        buttonDeleteAndAcceptedItem()
-        clickDelete()
-        clickAccepted()
-        inputText.value = ""
-        console.log("add item")
-    })
-}
-buttonAddItem()
+buttonAddTodo.addEventListener('click', () => { 
+    if (inputText.value === "") 
+    {
+        return alert("Please write any text")
+    }
+    todo = todo.concat([
+        {
+            todo: inputText.value
+        }
+    ])
+   
+    const newTodo = createNewTodo()
 
-buttonDeleteAndAcceptedItem = () => {
-    const listBuyAll= document.querySelectorAll(".todo__item")
+    todoList.appendChild(newTodo)
+    createTodoControls(newTodo) 
+
+    inputText.value = ""
+    })
+
+const createTodoControls = (todoItem) => {
 
     const allButton = document.createElement('div')
-    allButton.classList.add('todo__item-all')
+    allButton.classList.add('todo__item-all-button')
 
     const acceptedButton = document.createElement('button')
     acceptedButton.classList.add('todo__item-accepted')
-    acceptedButton.innerHTML = '<i class="fa-solid fa-check"></i>'
-    
+
+    const check = document.createElement('i')
+    check.className = ('fa-solid fa-check')
+     
     const deleteButton = document.createElement('button')
     deleteButton.classList.add('todo__item-delete')
-    deleteButton.innerHTML = '<i class="fa-solid fa-trash"></i>'  
-    
-        for ( listBuyAlly of listBuyAll) {
-            listBuyAlly.appendChild(allButton)
-            allButton.appendChild(acceptedButton)
-            allButton.appendChild(deleteButton)
-        }
-}
-buttonDeleteAndAcceptedItem()
 
-clickDelete = () =>{
-    const deleteButtonItem = document.querySelectorAll('.todo__item-delete')
+    const trash = document.createElement('i')
+    trash.className = ('fa-solid fa-trash')
         
-        for (const deleteButtonItems of deleteButtonItem){
-            deleteButtonItems.addEventListener("click", (event) => {
-                event.currentTarget.parentElement.parentElement.remove()
-                event.preventDefault()
-            })
-        }
-}
+    todoItem.appendChild(allButton)
+    allButton.appendChild(acceptedButton)
+    allButton.appendChild(deleteButton)
+    acceptedButton.appendChild(check)
+    deleteButton.appendChild(trash)
 
-clickAccepted = () =>{
-    const buttonAcceptedItem = document.querySelectorAll('.todo__item-accepted')
+    deleteButton.addEventListener('click', event => event.currentTarget.parentElement.parentElement.remove())
 
-        for (const buttonAcceptedItems of buttonAcceptedItem) {
-            buttonAcceptedItems.addEventListener('click', (event) =>{
-                event.currentTarget.parentElement.parentElement.classList.add('checked')
-                event.preventDefault()
-            })     
-        }
+    acceptedButton.addEventListener('click', event => event.currentTarget.parentElement.parentElement.classList.add('checked'))
 }
